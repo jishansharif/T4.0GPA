d31eab928967f8a3be58e67c6e058d9332f02a5a
Jishan Sharif <jishan@medsender.com>
2021-11-07 18:39:32 -0500

    WIP: Loading map when search by location

--- search.js
+++ search.js
@@ -1,6 +1,6 @@
-var form = document.getElementById('searchform');
+// var form = document.getElementById('searchform');
 // var location = document.getElementById("location");
-var display = document.getElementById("display");
+var display = document.getElementById("map");
 
 // form.addEventListener('submit', e => {
 //     e.preventDefault();
@@ -9,9 +9,9 @@ var display = document.getElementById("display");
 
 function getLocation(){
     
-    console.log(location);
-    console.log('---------------------');
-    console.log(form);
+    // console.log(location);
+    // console.log('---------------------');
+    // console.log(form);
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
       } else { 
@@ -20,24 +20,16 @@ function getLocation(){
 }
 
 function showPosition(position) {
-    x.innerHTML = "Latitude: " + position.coords.latitude + 
-    "<br>Longitude: " + position.coords.longitude;
+    // x.innerHTML = "Latitude: " + position.coords.latitude + 
+    // "<br>Longitude: " + position.coords.longitude;
+    const mymap = L.map('map').setView([position.coords.latitude, position.coords.longitude], 10);
+
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: 'pk.eyJ1IjoiYWhtMTM4OCIsImEiOiJja3ZwNWJtbDAwcDI2Mm5xdmdjeWFnbnA5In0.U-FY7XjdPoITkJElfGJKgQ'
+    }).addTo(mymap);
 }
-
-let map;
-
-function initMap() {
-  map = new google.maps.Map(document.getElementById("display"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
-}
-  
-
-// function showPosition(position) {
-//     var latlon = position.coords.latitude + "," + position.coords.longitude;
-
-//     var img_url = "https://maps.googleapis.com/maps/api/staticmap?center="+latlon+"&zoom=14&size=400x300&sensor=false&key=YOUR_KEY";
-
-//     document.getElementById("mapholder").innerHTML = "<img src='"+img_url+"'>";
-// }
